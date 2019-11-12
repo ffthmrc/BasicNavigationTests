2edf8dbb8ad9f4982e3290d4eef2c5e2c447b760
fatih meric <52293047+ffthmrc@users.noreply.github.com>
2019-11-12 15:22:14 -0500

    pass

---
+++ src/test/java/tests/D6_Testing/FirstTestNgTest.java
@@ -0,0 +1,16 @@
+package tests.D6_Testing;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNgTest {
+
+    @Test
+    public void test1(){
+        System.out.println("First test");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Secound test");
+
+    }
+}
